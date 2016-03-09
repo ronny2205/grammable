ae07cfc3f93c0dda6c6daef12cac45713e97fc34
Ronny Almog <ronny2205@gmail.com>
2016-03-09 13:45:31 +0000

    adding new action to controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root "grams#index"
+  resources :grams, only: [:new, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
